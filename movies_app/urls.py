a8062fbde6f258b7fc917470b58e36ad40098d9d
Sudarshan <sudarshan@testpress.in>
2021-09-17 16:07:45 +0530

    Fix mistake in urls

--- movies_app/urls.py
+++ movies_app/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("events.urls")),
+    path("", include("movies.urls")),
 ]
 
 
