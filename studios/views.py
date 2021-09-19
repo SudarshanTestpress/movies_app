e812524d863b7ec714808bc6d37ffb44639a1ec6
Sudarshan <sudarshan@testpress.in>
2021-09-19 20:46:27 +0530

    Add studio delete page

--- studios/views.py
+++ studios/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.contrib import messages
 
-# Create your views here.
+
+from .models import Studio
+
+
+class StudioCreateView(CreateView):
+    model = Studio
+    fields = ["title", "website"]
+    template_name = "studios/create_studio.html"
+    success_url = reverse_lazy("list_studio")
+
+
+class StudioListView(ListView):
+    model = Studio
+    paginate_by = 9
+    context_object_name = "studios"
+    template_name = "studios/studio_list.html"
+
+
+class StudioUpdateView(UpdateView):
+    model = Studio
+    fields = ["title", "website"]
+    template_name = "studios/create_studio.html"
+    success_url = reverse_lazy("list_studio")
+
+
+class StudioDeleteView(DeleteView):
+    model = Studio
+    template_name = "studios/delete_studio.html"
+    context_object_name = "studio"
+
+    def get_success_url(self):
+        messages.success(self.request, "Studio was deleted successfully.")
+        return reverse("list_studio")
