8ec7182512b5aa44b4aa4aee318c581074430ba6
Sudarshan <sudarshan@testpress.in>
2021-09-19 20:13:40 +0530

    Add list view for studios

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("studio/create", views.StudioCreateView.as_view(), name="create_studio"),
+    path("studio", views.StudioListView.as_view(), name="list_studio"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
 
 
 from .models import Studio
@@ -9,4 +10,11 @@ class StudioCreateView(CreateView):
     model = Studio
     fields = ["title", "website"]
     template_name = "movies/create_studio.html"
-    success_url = "#"
+    success_url = reverse_lazy("list_studio")
+
+
+class StudioListView(ListView):
+    model = Studio
+    paginate_by = 9
+    context_object_name = "studios"
+    template_name = "movies/studio_list.html"
