765ae4bec52c5d3098287eff6ebc0fc933c9805e
Sudarshan <sudarshan@testpress.in>
2021-09-21 10:18:12 +0530

    Change name from movies to studio

--- movies_app/settings.py
+++ movies_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "movies",
+    "studios",
     "taggit",
 ]
 
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "movies" / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "studios" / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
