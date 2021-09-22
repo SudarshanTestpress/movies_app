f2a943eb64327e940e542baf60462c330f4a3c30
Sudarshan <sudarshan@testpress.in>
2021-09-22 11:35:53 +0530

    Add director detail view

--- directors/views.py
+++ directors/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
 from django.urls import reverse_lazy, reverse
 from django.contrib import messages
 
 
-from studios.models import Director
+from studios.models import Director, Movies
 from .forms import DirectorCreateForm
 
 
@@ -36,3 +42,14 @@ class DirectorDeleteView(DeleteView):
     def get_success_url(self):
         messages.success(self.request, "Director Profile was deleted successfully.")
         return reverse("directors:list")
+
+
+class DirectorDetailView(DetailView):
+    model = Director
+    template_name = "directors/detail_director.html"
+    context_object_name = "director"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["movies"] = Movies.objects.filter(directors=self.kwargs.get("pk"))
+        return context
